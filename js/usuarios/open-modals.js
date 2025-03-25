f0bf7046b0db5fbdca3f4d18e07fd4c907f60280
OrlandoMartinho <srsaiombo@gmail.com>
2025-03-25 22:13:39 +0100

    finishid view of registrations

--- js/usuarios/open-modals.js
+++ js/usuarios/open-modals.js
@@ -1,9 +1,10 @@
+
 const openModalInscricao = async () => {
     
 
     try {
         // Carregar o modal dinamicamente
-        const response = await fetch("../../components/usuarios/modal-inscricao.html");
+        const response = await fetch("../../components/usuarios/editar-inscricao.html");
         if (!response.ok) {
             throw new Error(`Erro ao carregar o modal: ${response.statusText}`);
         }
@@ -41,6 +42,11 @@ function configurarModal() {
         }
     });
 }
+
+
+
+
+
 
 const openModalNotifications = async () => {
     
@@ -88,3 +94,55 @@ function configurarModalNotificacao() {
     });
 }
 
+
+
+
+
+
+const openModalDetalhes = async () => {
+    
+
+    try {
+        // Carregar o modal dinamicamente
+        const response = await fetch("../../components/usuarios/detalhes-modal.html");
+        if (!response.ok) {
+            throw new Error(`Erro ao carregar o modal: ${response.statusText}`);
+        }
+        const html = await response.text();
+        
+        // Inserir modal no container
+        const container = document.getElementById("inscricao-modal-container");
+        container.innerHTML = html;
+        
+        configurarModalDetalhes(); // Inicializa os eventos do modal
+    } catch (error) {
+        console.error("Erro ao carregar o modal:", error);
+    }
+};
+
+function configurarModalDetalhes() {
+    const modal = document.getElementById("detalhes-modal");
+    const fecharModal = document.getElementById("inscricao-closeModal");
+    
+    if (!modal) {
+        console.error("Modal não encontrado!");
+        return;
+    }
+    
+    modal.showModal(); // Abre o modal automaticamente ao ser carregado
+    
+    if (fecharModal) {
+        fecharModal.addEventListener("click", () => modal.close());
+    }
+
+    // Fechar modal ao clicar fora
+    modal.addEventListener("click", (event) => {
+        if (event.target === modal) {
+            modal.close();
+        }
+    });
+}
+
+function closeModal() {
+    document.getElementById("detalhes-modal").close();
+}
