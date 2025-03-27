7f0bba090ae310497467333472a58873e7ef16e4
OrlandoMartinho <srsaiombo@gmail.com>
2025-03-27 21:00:33 +0100

    updating web page

--- js/sessoes/login.js
+++ js/sessoes/login.js
@@ -6,11 +6,9 @@ document.getElementById("entrar").addEventListener("click", function (event) {
     const senha = document.getElementById("senha").value;
 
     // Simulação de autenticação (Substitua por uma requisição real à API)
-    if (email.includes("avaliador")) {
+    if (email =="admin@gmail.com"&&senha=="12345678") {
         window.location.href = "../../pages/avaliadores/avaliador-home.html"
-    } else if(email.includes("admin")){
-        window.location.href = "../../pages/admin/admin-home.html";
-    }else{
-        window.location.href = "../../pages/usuarios/user-home.html";
+    } else{
+        alert("Credenciais inválidas ,por favor tente novamente")
     }
 });
